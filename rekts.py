f3154c1c8a82c4b41fb5a8a0c6e8121221000ecf
Ben Line <13621351+0xlino@users.noreply.github.com>
2023-12-29 00:24:08 +0000

    Update

--- rekts.py
+++ rekts.py
@@ -81,7 +81,7 @@ def make_title(filename):
                 
 def download_project_images():
     # with open('1-small.json', 'r') as f:
-    with open('1-medium.json', 'r') as f:
+    with open('rektdb.json', 'r') as f:
         data = json.load(f)
         items = data['items']
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
